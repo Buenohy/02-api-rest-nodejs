43a736ee27bc8eefb78ccfce87f0a12cbdfe42bf
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-04-28 05:12:01 -0300

    feat: Creating List and Get transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,9 +4,29 @@ import { randomUUID } from 'node:crypto'
 import { knex } from '../database.js'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.post('/', async (request, reply) => {
-    // { title, amount, type: credit ou debit }
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select()
 
+    return {
+      transactions,
+    }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
+  app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
       amount: z.number(),
