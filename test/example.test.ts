7b331faca19878d6e06c62e6cbd23afd0898c461
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-04-28 16:11:33 -0300

    feat: install types to supertest

--- test/example.test.ts
+++ test/example.test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from 'vitest'
-import { app } from '../src/server.js'
+import request from 'supertest'
+import { app } from '../src/app.js'
 
 test('o usuário consegue criar uma nova transação', () => {
   // fazer a chamada HTTP p/ criar uma nova transação
