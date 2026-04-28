b78b5a1b6fefdab8ddd912756d7aabc58a58dd9e
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-04-28 03:24:56 -0300

    feat: variables envirionment

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,14 @@
+import 'dotenv/config'
 import setupKnex, { type Knex } from 'knex'
+
+if (!process.env.DATABASE_UL) {
+  throw new Error('DATABASE_UL env not found.')
+}
 
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './db/app.db',
+    filename: process.env.DATABASE_UL,
   },
   useNullAsDefault: true,
   migrations: {
