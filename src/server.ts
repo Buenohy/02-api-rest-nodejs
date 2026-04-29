22fe9c1d2b6c5cf76f3d13a34a4495341c530705
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2026-04-29 13:41:25 -0300

    fix: Port to deploy on render

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import { app } from './app.js'
 import { env } from './env/index.js'
 
+const port = Number(process.env.PORT) || 3000
+
 app
   .listen({
-    port: env.PORT,
+    port,
+    host: '0.0.0.',
   })
   .then(() => {
     console.log('HTTP Server Running!')
